dce9468be12c0023f1a6eb3fcd0ad71b872cd339
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2024-10-28 14:17:46 +0500

    learning about modules in python

---
+++ modules/app.py
@@ -0,0 +1,7 @@
+from mymodule import greet
+from my_package import math
+
+
+greet("Sajjad")
+
+print(math.add(1, 2, 3))
---
+++ modules/my_package/math.py
@@ -0,0 +1,5 @@
+def add(*numbers):
+    sum = 0
+    for n in numbers:
+        sum += n
+    return sum
