ec650a057f02d6cc3a09188c1907b0ed3fe9e69a
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2024-10-29 18:04:51 +0500

    Designed Text Editor and load ui in code

---
+++ pyqt/texteditor/app.py
@@ -0,0 +1,18 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.uic import loadUi
+
+import sys
+
+class Main(QMainWindow):
+
+    def __init__(self):
+        super(Main, self).__init__()
+        loadUi("main.ui", self)
+
+    
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    ui = Main()
+    ui.show()
+    app.exec_()
