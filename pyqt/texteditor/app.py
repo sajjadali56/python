8882b76f8717c731519bd269cec15a3093340869
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2024-10-29 18:22:51 +0500

    Attached signals to slots

--- pyqt/texteditor/app.py
+++ pyqt/texteditor/app.py
@@ -9,6 +9,61 @@ class Main(QMainWindow):
         super(Main, self).__init__()
         loadUi("main.ui", self)
 
+        self.actionNew.triggered.connect(self.newFile)
+        self.actionSave.triggered.connect(self.saveFile)
+        self.actionSave_As.triggered.connect(self.saveFileAs)
+        self.actionOpen.triggered.connect(self.openFile)
+        self.actionUndo.triggered.connect(self.undo)
+        self.actionRedo.triggered.connect(self.redo)
+        self.actionCut.triggered.connect(self.cut)
+        self.actionCopy.triggered.connect(self.copy)
+        self.actionPaste.triggered.connect(self.paste)
+        self.actionSet_Dark_Mode.triggered.connect(self.setDarkMode)
+        self.actionSet_Light_Mode.triggered.connect(self.setLightMode)
+        self.actionIncrease_Font_Size.triggered.connect(self.incFontSize)
+        self.actionDecrease_Font_Size.triggered.connect(self.decFontSize)
+
+
+    def newFile(self):
+        print("new file")
+    
+    def saveFile(self):
+        ""
+
+    def saveFileAs(self):
+        ""
+    
+    def openFile(self):
+        ""
+    
+    def undo(self):
+        ""
+
+    def redo(self):
+        ""
+    
+    def cut(self):
+        ""
+    
+    def copy(self):
+        ""
+    
+    def paste(self):
+        ""
+    
+    def setDarkMode(self):
+        ""
+
+    def setLightMode(self):
+        ""
+    
+    def incFontSize(self):
+        ""
+    
+    def decFontSize(self):
+        ""
+
+
     
 if __name__ == "__main__":
 
