d5be9b88e67c12bf793bd2b3e67b99618d9ef722
Sajjad Ali <sajjad.ali@badriconsultancy.com>
2024-12-03 17:05:23 +0500

    Pandas demo for reading excel files

---
+++ pandas-demo/excel.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+path = "C:\\Users\\SajjadAli\\Documents\\Codes\\Sanjay\\IFRS_22July\\GMM\\GMM_Input_v5.xlsx"
+data = pd.read_excel(path)
+
+print(data.head())
